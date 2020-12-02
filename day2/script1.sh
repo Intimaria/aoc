ceddb9f63c13a00d4e22d95ced54ee8699bdd1bc
Intimaria <intimaria@gmail.com>
2020-12-02 13:56:08 +0000

    Add new file

---
+++ day2/script1.sh
@@ -0,0 +1,24 @@
+#!/usr/local/bin/bash
+
+
+suma=0
+
+while read i;
+do
+letter=$(echo $i | cut -d' ' -f2 | tr -d ':' )
+pass=$(echo $i  | cut -d' ' -f3 )
+
+min=$(echo $i | cut -d'-' -f1 )
+max=$(echo $i | cut -d'-' -f2 | cut -d' ' -f1)
+num=$( tr -dc "$letter" <<< "$pass" | tr -d ' ' | wc -c )
+if [[ $num -ge $min ]] && [[ $num -le $max ]]; then
+	let suma++
+fi
+
+
+done < input.txt
+
+if [[ $suma -ne 0 ]]; then
+echo $suma
+fi 
+exit 0
