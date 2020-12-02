2c0ff197a699582c0192d250efa5b531e73bddc3
Intimaria <intimaria@gmail.com>
2020-12-02 14:22:24 +0000

    Update script1.sh

--- day2/script1.sh
+++ day2/script1.sh
@@ -5,20 +5,16 @@ suma=0
 
 while read i;
 do
-letter=$(echo $i | cut -d' ' -f2 | tr -d ':' )
-pass=$(echo $i  | cut -d' ' -f3 )
+	letter=$(echo $i | cut -d' ' -f2 | tr -d ':' )
+	pass=$(echo $i  | cut -d' ' -f3 )
+	min=$(echo $i | cut -d'-' -f1 )
+	max=$(echo $i | cut -d'-' -f2 | cut -d' ' -f1)
+	num=$( tr -dc "$letter" <<< "$pass" | tr -d ' ' | wc -c )
 
-min=$(echo $i | cut -d'-' -f1 )
-max=$(echo $i | cut -d'-' -f2 | cut -d' ' -f1)
-num=$( tr -dc "$letter" <<< "$pass" | tr -d ' ' | wc -c )
-if [[ $num -ge $min ]] && [[ $num -le $max ]]; then
-	let suma++
-fi
-
-
+	if [[ $num -ge $min ]] && [[ $num -le $max ]]; then
+		let suma++
+	fi
 done < input
 
-if [[ $suma -ne 0 ]]; then
 echo $suma
-fi 
 exit 0
