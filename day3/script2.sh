5ef9d8877e7b62b7c5e185dff126cad53cde3022
Intimaria <intimaria@gmail.com>
2020-12-07 04:52:58 +0000

    Update script2.sh

--- day3/script2.sh
+++ day3/script2.sh
@@ -1,10 +1,12 @@
 #!/usr/local/bin/bash
 
+# I already know the length of the line from part 1 (saves time)
+length=31
 index=0
 trees=0
 
-length=31
 find() {
+# second condition in while makes process faster dunno why
 while read -r line  && [[ $2 -lt $length ]]; do
 		if [[ $index -ge $length ]]; then
 			let index=index-$length
@@ -12,16 +14,15 @@ while read -r line  && [[ $2 -lt $length ]]; do
 		if [[ ${line:$index:1} == '#' ]]; then	
 			let trees++
 		fi
-	index=$(($index+$1))
-	
+	index=$(($index+$1))	
 done < input3.txt 
 echo "$trees" 
 }
 
 find_two() {
+# delete every two lines keep even lines in array
 file=$( cat input3.txt | sed -n 'p;n' );
 for line in ${file[@]}; do
-
 		if [[ $index -ge $length ]]; then
 			let index=index-$length
 		fi
@@ -29,23 +30,13 @@ for line in ${file[@]}; do
 			let trees++
 		fi
 	index=$(($index+$1))
-	
 done < input3.txt 
 echo "$trees" 
 }
-trees7=$(find 7 1)
-echo "$trees7"
-
-trees5=$(find 5 1)
-echo "$trees5"
-
-trees3=$(find 3 1)
-echo "$trees3"
 
 trees1=$(find 1 1)
-echo "$trees1"
-
 trees2=$(find_two 1 2)
-echo "$trees2"
-
-echo $(($trees1*$trees3*$trees5*$trees7*$trees2))
+trees3=$(find 3 1)
+trees5=$(find 5 1)
+trees7=$(find 7 1)
+echo $(($trees1*$trees2*$trees3*$trees5*$trees7))
