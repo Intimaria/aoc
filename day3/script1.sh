8e6f8f88038f3e85c4bcc4bd19b2b456a8318a59
Intimaria <intimaria@gmail.com>
2020-12-07 04:17:58 +0000

    Update script1.sh

--- day3/script1.sh
+++ day3/script1.sh
@@ -1,9 +1,9 @@
 #!/usr/local/bin/bash
 index=0
 trees=0
+length=$( echo ${line[*]} | wc -c )
+let length--
 while read -r line; do
-	length=$( echo ${line[*]} | wc -c )
-	let length--
 	if [[ $index -ge $length ]]; then
 		let index=index-$length
 	fi
