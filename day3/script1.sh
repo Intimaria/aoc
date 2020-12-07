e0570e505ca3cc80a86baa71796cabaa07629a76
Intimaria <intimaria@gmail.com>
2020-12-07 04:13:48 +0000

    Add new file

---
+++ day3/script1.sh
@@ -0,0 +1,15 @@
+#!/usr/local/bin/bash
+index=0
+trees=0
+while read -r line; do
+	length=$( echo ${line[*]} | wc -c )
+	let length--
+	if [[ $index -ge $length ]]; then
+		let index=index-$length
+	fi
+	if [[ ${line:$index:1} == '#' ]]; then
+		let trees++
+	fi
+	index=$(($index+3))
+done < input3.txt 
+echo "$trees"
